8b70c96eaa747fbc14f153da88159f15cbc4f1af
Aris Winardi <me@arwin.my.id>
2015-09-07 14:10:48 +0700

    update

--- get_data_from_newsd.php
+++ get_data_from_newsd.php
@@ -2,76 +2,36 @@
 include ("static/inc/con.php");
 include ("static/inc/function.php");
 include ("static/inc/conf.php");
-include ("form/simple_html_dom.php");
 
 
-$target 	= "http://202.146.128.250:8222/search.php?search=$DefaultSearch";
-$html 		= file_get_html($target);
+$target = "http://202.146.128.250:8222/xml.php?search=$DefaultSearch&limit=100";
 
-$kode			="";
-$media			="";
-$title			="";
-$date 			="";
-$news_content 	="";
-$writer			="";
-$image 			="";
-$url 			="";
 
-	foreach($html->find('div[itemprop=count_data]') as $a){
-		$count = $a->plaintext;
-	}
-	
-	foreach($html->find('div[itemprop=news_data]') as $news_data){
-		$kode = $news_data->id;
-		
-		foreach($news_data->find('div[itemprop=title]') as $b){
-			$title = $b->plaintext;
-		}
-	
-		foreach($news_data->find('div[itemprop=date]') as $c){
-			$date = $c->plaintext;
-		}
-	
-		//foreach($news_data->find('div[itemprop=news_content]') as $d){
-			//$news = $d->plaintext;
-		//}	
-		
-		foreach($news_data->find('div[itemprop=writer]') as $e){
-			$writer = $e->plaintext;
-		}
-	
-		foreach($news_data->find('div[itemprop=image]') as $f){
-			$image = $f->plaintext;
-		}
-	
-		foreach($news_data->find('div[itemprop=url]') as $g){
-			$url = $g->plaintext;
-		}
-		
-		foreach($news_data->find('div[itemprop=media]') as $h){
-			$media = $h->plaintext;
-		}
-		
-		$target2 = "http://202.146.128.250:8222/get_content_db.php?kode=$kode";
-		$html2 	= file_get_html($target2);
-		$news = $html2->find('news',0)->plaintext;
-		
-		
+	$xml = simplexml_load_file($target);
+
+	foreach($xml->item as $item){
+		$kode =  $item->kode;
+		$media =  $item->media;
+		$title =  $item->title;
+		$date =  $item->date;
+		$link =  $item->link;
+		$image =  $item->image;
+		$writer =  $item->writer;
+		$content =  $item->content;
 		
 		//echo "Kode:".$kode.PHP_EOL;
 		//echo "media:".$media.PHP_EOL;
 		//echo "title:".$title.PHP_EOL;
 		//echo "date:".$date.PHP_EOL;
-		//echo "news content:".$news.PHP_EOL;
 		//echo "writer:".$writer.PHP_EOL;
-		//echo "url:".$url.PHP_EOL;
+		//echo "url:".$link.PHP_EOL;
 		//echo "image:".$image.PHP_EOL;
-		//echo "time:".$NOW.PHP_EOL;
-		echo $NOW."-------------------------------------------------------------------------------------------------------".PHP_EOL;
-		save_data_from_newsd($DefaultSearch,$kode,$media,$title,$date,$news,$writer,$url,$image,$NOW);
-		
+		//echo "content:".$content.PHP_EOL;
+		//echo $NOW."-------------------------------------------------------------------------------------------------------".PHP_EOL;
+		save_data_from_newsd($DefaultSearch,$kode,$media,$title,$date,$content,$writer,$link,$image,$NOW);
 		
 		
 	}
+	
 
 ?>
--- static/inc/conf.php
+++ static/inc/conf.php
@@ -2,14 +2,14 @@
 $NAME 			= Settings("Name");
 $Version 		= Settings("Version");
 $Description 	= Settings("Description");
-$DefaultSearch = (string)Settings("DefaultSearch");
+$DefaultSearch 	= Settings("DefaultSearch");
 $DataPerPage 	= Settings("DataPerPage");
-$TimeOut			= Settings("TimeOut");
+$TimeOut		= Settings("TimeOut");
 
 
-$NO0 				= 0;
-$NO1 				= 1;
-$NOW 				= date("Y-m-d H:i:s");
+$NO0 			= 0;
+$NO1 			= 1;
+$NOW 			= date("Y-m-d H:i:s");
 $TODAY 			= date("Y-m-d");
 
 
